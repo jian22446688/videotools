b09b586f9d337599c9caa3e49436a8ec3be97c41
Cary <798171920@qq.com>
2018-06-14 08:33:12 +0800

    添加截图

--- src/main/index.js
+++ src/main/index.js
@@ -25,7 +25,7 @@ function createWindow () {
   })
 
   mainWindow.loadURL(winURL),
-      mainWindow.webContents.openDevTools(),
+//      mainWindow.webContents.openDevTools(),
   mainWindow.on('closed', () => {
     mainWindow = null
   })
